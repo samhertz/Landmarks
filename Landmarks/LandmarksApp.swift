9490e0d093046c35b67f38fd0db07bf09d37eb86
Sam Hertz <samhertz@vt.edu>
2022-06-20 18:58:46 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Samuel Hertz on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
